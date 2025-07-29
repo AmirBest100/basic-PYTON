c1d7b083305482e404c6c7c63814a21465a7d413
Amir_the_Best <amir.is.power.and.best@gmail.com>
2025-07-29 23:28:23 +0330

    Add files via upload
    basics of python

---
+++ basic_1.py
@@ -0,0 +1,23 @@
+# data types :
+# 1) String(str)
+# 2) Number(int)
+# 3) Boolean(bool)
+# 4) None(empty)
+# 5) Undefined(no type defined)
+# 6) ...
+# ============================================================
+# Print
+
+
+print ("Goodbye World !")
+# data type : String(str) ⬆⬆⬆
+
+print (1234567890)
+# data type : Number(int) ⬆⬆⬆
+
+print (True)
+print (False)
+# data type : Boolean(bool) ⬆⬆⬆
+
+print ()
+# data type : None(empty) ⬆⬆⬆
---
+++ basic_2.py
@@ -0,0 +1,40 @@
+# variable
+a = 1
+b = "boom 100"
+c = None
+dainasor = False
+
+# constant 
+X = "20"
+Z = True
+Y = 1254
+FISH = None
+
+# ================================================
+
+#LISTs
+my_list = ["",123,"hello",None,"hello again 10101010"]
+print("\n")
+print(my_list)
+print(len(my_list))
+my_list.append("Nice !")
+my_list.remove(123)
+my_list.insert(3,"WOW")
+# my_list.insert(<index>,<anything>)
+print("\n")
+print(my_list)
+
+print("\n")
+print(my_list[2]) 
+print(my_list[2:4]) 
+print(my_list[:3]) 
+print(my_list[3:]) 
+print(my_list[::2]) 
+print(my_list[::-1]) 
+# print(my_list[ <start> : <end> ]) 
+# print(my_list[:: <step> ]) 
+
+# ===========================================
+print("\n") # => one Enter
+print("\t") # => one Tab
+print("\\") # => the \
